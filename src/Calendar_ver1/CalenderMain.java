86a3d653d2ed5c3f3ff598adbcadd4ba34df99de
USER <USER@DESKTOP-MODERN14>
2022-08-16 11:31:26 +0900

    스텝1.

--- src/Calendar_ver1/CalenderMain.java
+++ src/Calendar_ver1/CalenderMain.java
@@ -4,6 +4,15 @@ public class CalenderMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Calendar!");
-	}
 
+		// step1.
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+
+	}
+	// ctrl + shift + f = 엉망 탭 자동 정렬~~
 }
